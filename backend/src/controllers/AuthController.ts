6f55cf71dd4d17ae6a3ae77bf2328da17082886b
KiritoEM <johankirito64@gmail.com>
2024-12-07 22:55:59 +0300

    feat: add authRouter and handling it

---
+++ backend/src/controllers/AuthController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class AuthController {
+    async register(req: Request, res: Response) {
+        
+    }
+}
+
+export default new AuthController();
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import AuthController from "@/controllers/AuthController";
+import { erroHandler as err_hdl } from "@/middlewares/error";
 
 const authRouter: Router = Router();
 
-authRouter.post("/auth");
+authRouter.post("/register", err_hdl(AuthController.register));
 
 export default authRouter;
