5da144964c43d6c1ed5db770b4511e581cd29dbf
KiritoEM <johankirito64@gmail.com>
2024-12-12 08:52:06 +0300

    chore: add routes to react

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+    <BrowserRouter>
+        <Routes>
+            <Route index />
+        </Routes>
+    </BrowserRouter>
+}
+
+export default AppRoutes;
