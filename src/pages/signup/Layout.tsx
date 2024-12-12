6c822d57de408d43f616325011315e330fa573da
KiritoEM <johankirito64@gmail.com>
2024-12-12 19:44:19 +0300

    feat: add responsive to signup and Routing

--- src/pages/signup/Layout.tsx
+++ src/pages/signup/Layout.tsx
@@ -9,8 +9,8 @@ interface SignupLayoutProps extends RootLayout { }
  */
 const SignupLayout: FC<SignupLayoutProps> = ({ children }): JSX.Element => {
     return (
-        <section className="signup w-full overflow-x-hidden bg-[url('/images/signup-bg.png')] min-h-screen bg-cover" id="login">
-            <div className="signup-container container mx-auto px-6 md:px-14 xl:px-20 2xl:px-32 flex flex-col lg:flex-row justify-center lg:gap-16 item-center min-h-screen">
+        <section className="signup w-full overflow-x-hidden bg-white md:bg-[url('/images/signup-bg.png')] min-h-screen bg-cover" id="login">
+            <div className="signup-container container mx-auto px-0 md:px-14 xl:px-20 2xl:px-32 flex flex-col lg:flex-row justify-center lg:gap-16 item-center min-h-screen">
                 {children}
             </div>
         </section>
