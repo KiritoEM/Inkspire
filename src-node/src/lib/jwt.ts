053ea2c76d619f856630dea4889104fe7c46e67b
KiritoEM <johankirito64@gmail.com>
2024-12-13 12:41:50 +0300

    feat: add email__token_expiration -> back

--- src-node/src/lib/jwt.ts
+++ src-node/src/lib/jwt.ts
@@ -2,23 +2,24 @@ import { TOKEN_EXPIRED, TOKEN_SECRET } from "@/helpers/constants";
 import { JwtPayload, sign, verify } from "jsonwebtoken";
 
 /**
- * Creates a new JWT token with given data and token secret.
- * @param data the payload that will be encrypted in the JWT token
- * @param tokenSecret the secret key that will be used to sign the token
- * @returns the JWT token or an error if the data is empty
+ * Creates a JSON Web Token (JWT) given the payload data
+ * @param data the payload data to be encoded into the JWT
+ * @param tokenSecret the secret key that will be used to create the JWT
+ * @param tokenExpiration the expiration time of the JWT
+ * @returns a Promise with the JWT string or an error if the data is empty
  */
-const createJWT = (data: JwtPayload, tokenSecret?: string) => {
+const createJWT = (data: JwtPayload, tokenSecret?: string, tokenExpiration?: any) => {
     if (Object.keys(data).length === 0) {
         return new Error("Data cannot be empty for payload !!!");
     }
 
     const secret = tokenSecret ?? TOKEN_SECRET;
-    return sign(data, secret, TOKEN_EXPIRED);
+    const expiration = tokenExpiration ?? TOKEN_SECRET;
+    return sign(data, secret, expiration);
 };
 
 /**
  * Verifies a JWT token given the token and a secret key.
- * If the given secret key is empty, it will use the default secret key.
  * @param token the JWT token to verify
  * @param tokenSecret the secret key that will be used to verify the token
  * @returns the decoded payload of the JWT token or an error if the token is invalid
