3883591f47874e5c79e8fbd94f274244f7cce257
KiritoEM <johankirito64@gmail.com>
2024-12-20 21:42:03 +0300

    feat: add send_request namespace (socket)  -> back

--- src-node/src/socket/handlers/userHandlers.ts
+++ src-node/src/socket/handlers/userHandlers.ts
@@ -3,7 +3,7 @@ import { Namespace, Socket } from "socket.io";
 
 const userHandlers = (io: Namespace<any>, socket: Socket, socketIdMap: { [userId: string]: string }) => {
     const sendRequest = async (senderId: number, receivedId: number) => {
-        const senderUser = await UserServices.readUserById(senderId);
+        const senderUser = await UserServices.readUserById(Number(senderId));
 
         io.to(socketIdMap[receivedId]).emit("user:send_request", JSON.stringify(senderUser));
     }
