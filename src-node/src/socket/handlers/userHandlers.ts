6c7e14a0a91d2e8741d921cf6bb2f64a9c55c4e8
KiritoEM <johankirito64@gmail.com>
2024-12-21 16:34:43 +0300

    feat: add confirm_request namespace (socket) -> back

--- src-node/src/socket/handlers/userHandlers.ts
+++ src-node/src/socket/handlers/userHandlers.ts
@@ -4,22 +4,22 @@ import { Namespace, Socket } from "socket.io";
 /**
  * Handles user socket events.
  *
- * @param {Namespace} io - The socket io namespace
+ * @param {Namespace} namespace - The socket io namespace
  * @param {Socket} socket - The socket object
  * @param {Record<string, string>} socketIdMap - A map of user IDs to socket IDs
  */
-const userHandlers = (io: Namespace<any>, socket: Socket, socketIdMap: { [userId: string]: string }) => {
+const userHandlers = (namespace: Namespace<any>, socket: Socket, socketIdMap: { [userId: string]: string }) => {
     const sendFollowRequest = async (senderId: number, receivedId: number) => {
         const senderUser = await UserServices.readUserById(Number(senderId));
 
-        io.to(socketIdMap[receivedId]).emit("user:send_request", JSON.stringify(senderUser));
+        namespace.to(socketIdMap[receivedId]).emit("user:send_request", JSON.stringify(senderUser));
     }
 
     const confirmFollowRequest = async (requestId: number) => {
         const follow_request = await UserServices.readFollower_requestById(Number(requestId));
         console.log(follow_request, socketIdMap[follow_request?.senderId as number]);
 
-        io.to(socketIdMap[follow_request?.senderId as number]).emit("user:confirm_follow_request", JSON.stringify(follow_request?.receiver));
+        namespace.to(socketIdMap[follow_request?.senderId as number]).emit("user:confirm_follow_request", JSON.stringify(follow_request?.receiver));
     }
 
     socket.on("user:send_follow_request", sendFollowRequest);
